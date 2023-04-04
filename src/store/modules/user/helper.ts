5a4673cb175282c34dedfc5382d05be8fc4330d6
siykt <cnsiykt@163.com>
2023-04-04 19:53:36 +0800

    feat: use my avatar and name

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://p3-passport.byteimg.com/img/user-avatar/f32b9d98ae0d14030684fb9d156f21c6~180x180.awebp',
+      name: 'AntPro',
+      description: 'AntPro的ChatGPT',
     },
   }
 }
